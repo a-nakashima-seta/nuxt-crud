91edd6e92b32fcb1ce310d161aff0f6b9375d6d6
a-nakashima-seta <a-nakashima@s-shizensyokuhin.co.jp>
2025-05-20 18:16:32 +0900

    ホーム画面_共通ヘッダ・サイド追加

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,6 +2,6 @@
 export default defineNuxtConfig({
   compatibilityDate: '2024-11-01',
   devtools: { enabled: true },
-  modules: ["@prisma/nuxt", '@nuxt/ui'],
-    css: ['~/public/assets/css/main.css']
+  modules: ["@prisma/nuxt", '@nuxt/ui', '@nuxt/icon'],
+  css: ['~/public/assets/css/main.css']
 })
